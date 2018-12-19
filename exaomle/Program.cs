a96e646f993722452b67fbc756b2a17286c4bee1
Maxim Morozyouk <gonnatestyourstuff@gmail.com>
2018-12-19 15:53:44 +0200

    pushing some code from lecture or homework

---
+++ exaomle/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Serilog;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Configuration;
+
+namespace SimpleSink
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.MySink()
+                .CreateLogger();
+
+            var position = new { Latitude = 25, Longitude = 134 };
+            var elapsedMs = 34;
+
+            log.Information("Processed {@Position} in {Elapsed:000} ms.", position, elapsedMs);
+
+        }
+    }
+
+    public class MySink : ILogEventSink
+    {
+        private readonly IFormatProvider _formatProvider;
+
+        public MySink(IFormatProvider formatProvider)
+        {
+            _formatProvider = formatProvider;
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            var message = logEvent.RenderMessage(_formatProvider);
+            Console.WriteLine(DateTimeOffset.Now.ToString() + " " + message);
+        }
+    }
+
+    public static class MySinkExtensions
+    {
+        public static LoggerConfiguration MySink(
+                  this LoggerSinkConfiguration loggerConfiguration,
+                  IFormatProvider formatProvider = null)
+        {
+            return loggerConfiguration.Sink(new MySink(formatProvider));
+        }
+    }
+}
